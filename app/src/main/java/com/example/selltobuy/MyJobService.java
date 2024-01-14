2ce560aa747fb9611c9b50ab6b89d45057912bbf
liam shmilovich <liam.shmilovich@gmail.com>
2024-01-14 12:07:12 +0200

    final 14/1/24

---
+++ app/src/main/java/com/example/selltobuy/MyJobService.java
@@ -0,0 +1,21 @@
+package com.example.selltobuy;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+public class MyJobService extends JobService {
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+ //       MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.gong);
+//        mediaPlayer.start();
+        Toast.makeText(getApplicationContext(), "good", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+}
